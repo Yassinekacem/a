7cb06397e570074a5907631fc6aaed469c6c92a2
Yassine Kacem <kacemyassine740@gmail.com>
2026-01-05 12:14:42 +0100

    Handle NaN values in target before training

--- steps/train.py
+++ steps/train.py
@@ -1,52 +1,62 @@
+# steps/train.py
 import os
 import joblib
 import yaml
 from sklearn.preprocessing import StandardScaler, OneHotEncoder, MinMaxScaler
 from sklearn.compose import ColumnTransformer
 from imblearn.over_sampling import SMOTE
-from imblearn.pipeline import Pipeline 
+from imblearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 from sklearn.tree import DecisionTreeClassifier
+
 
 class Trainer:
     def __init__(self):
         self.config = self.load_config()
-        self.model_name = self.config['model']['name']
-        self.model_params = self.config['model']['params']
-        self.model_path = self.config['model']['store_path']
+        self.model_name = self.config["model"]["name"]
+        self.model_params = self.config["model"]["params"]
+        self.model_path = self.config["model"]["store_path"]
         self.pipeline = self.create_pipeline()
 
     def load_config(self):
-        with open('config.yml', 'r') as config_file:
+        with open("config.yml", "r") as config_file:
             return yaml.safe_load(config_file)
-        
+
     def create_pipeline(self):
-        preprocessor = ColumnTransformer(transformers=[
-            ('minmax', MinMaxScaler(), ['AnnualPremium']),
-            ('standardize', StandardScaler(), ['Age','RegionID']),
-            ('onehot', OneHotEncoder(handle_unknown='ignore'), ['Gender', 'PastAccident']),
-        ])
-        
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ("minmax", MinMaxScaler(), ["AnnualPremium"]),
+                ("standardize", StandardScaler(), ["Age", "RegionID"]),
+                ("onehot", OneHotEncoder(handle_unknown="ignore"), ["Gender", "PastAccident"]),
+            ]
+        )
+
         smote = SMOTE(sampling_strategy=1.0)
-        
+
         model_map = {
-            'RandomForestClassifier': RandomForestClassifier,
-            'DecisionTreeClassifier': DecisionTreeClassifier,
-            'GradientBoostingClassifier': GradientBoostingClassifier
+            "RandomForestClassifier": RandomForestClassifier,
+            "DecisionTreeClassifier": DecisionTreeClassifier,
+            "GradientBoostingClassifier": GradientBoostingClassifier,
         }
-    
+
         model_class = model_map[self.model_name]
         model = model_class(**self.model_params)
 
-        pipeline = Pipeline([
-            ('preprocessor', preprocessor),
-            ('smote', smote),
-            ('model', model)
-        ])
+        pipeline = Pipeline(
+            steps=[
+                ("preprocessor", preprocessor),
+                ("smote", smote),
+                ("model", model),
+            ]
+        )
 
         return pipeline
 
     def feature_target_separator(self, data):
+        # Drop rows where the target (last column) is NaN
+        target_col = data.columns[-1]
+        data = data.dropna(subset=[target_col])
+
         X = data.iloc[:, :-1]
         y = data.iloc[:, -1]
         return X, y
@@ -55,5 +65,6 @@ class Trainer:
         self.pipeline.fit(X_train, y_train)
 
     def save_model(self):
-        model_file_path = os.path.join(self.model_path, 'model.pkl')
+        os.makedirs(self.model_path, exist_ok=True)
+        model_file_path = os.path.join(self.model_path, "model.pkl")
         joblib.dump(self.pipeline, model_file_path)
